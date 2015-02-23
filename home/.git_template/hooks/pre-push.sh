8ce42f4ff07429e5df68130a787be77457bb330c
Hammad Khalid <hammad.khalid@shopify.com>
2015-02-23 16:38:36 +0000

    Simplify the code for pre-hook

--- home/.git_template/hooks/pre-push.sh
+++ home/.git_template/hooks/pre-push.sh
@@ -8,14 +8,13 @@ PUSH_COMMAND=`ps -ocommand= -p $PPID`
 PROTECTED_BRANCHES="^(master|dev|release-*|patch-*)"
 FORCE_PUSH="force|delete|-f"
 
-echo "Pre hook was run"
 if [[ "$BRANCH" =~ $PROTECTED_BRANCHES ]]; then
   read -p "You're about to push to \"$BRANCH\", are you sure you to continue (y/n)" -n 1 -r < /dev/tty
   echo
-  if [[ $REPLY =~ ^[Yy]$ ]]
-  then
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
     exit 0
   fi
+  echo "Push aborted."
   exit 1
 fi
 
