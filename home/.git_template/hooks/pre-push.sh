ad9bba3b65992cb69ce7e2ce94d4eed877196f24
Hammad Khalid <hammad.khalid@shopify.com>
2015-02-23 16:06:33 +0000

    Update the prompt message in pre-push hook

--- home/.git_template/hooks/pre-push.sh
+++ home/.git_template/hooks/pre-push.sh
@@ -8,7 +8,7 @@ PROTECTED_BRANCHES="^(master|dev|release-*|patch-*)"
 FORCE_PUSH="force|delete|-f"
 
 if [[ "$BRANCH" =~ $PROTECTED_BRANCHES && "$PUSH_COMMAND" =~ $FORCE_PUSH ]]; then
-  echo "Prevented force-push to protected branch \"$BRANCH\" by pre-push hook"
+  echo "Are you sure you want force push to \"$BRANCH\" ?"
   exit 1
 fi
 
